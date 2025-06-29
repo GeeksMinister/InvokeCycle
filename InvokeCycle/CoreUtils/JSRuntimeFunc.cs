c2fae450f5315f90dde6c1b066a5d442e7ffc5c6
NAEL SHARABI <nael_sps@hotmail.com>
2025-06-29 08:06:33 +0200

    Created experiment 2. Added Tabs.

---
+++ InvokeCycle/CoreUtils/JSRuntimeFunc.cs
@@ -0,0 +1,31 @@
+﻿namespace InvokeCycle.CoreUtils.Extensions;
+
+public static class JSRuntimeFunc
+{
+    //Local Storage
+    public static async Task LocalStorage_Clear(this IJSRuntime JS)
+        => await JS.InvokeVoidAsync("localStorage.clear");
+    public static async Task LocalStorage_RemoveItem(this IJSRuntime JS, string key)
+        => await JS.InvokeVoidAsync("localStorage.removeItem", key);
+    public static async Task LocalStorage_SetItem(this IJSRuntime JS, string key, object value)
+        => await JS.InvokeVoidAsync("localStorage.setItem", key, value);
+    public static async Task LocalStorage_SetSerializedItem<T>(this IJSRuntime JS, string key, T value)
+        => await JS.InvokeVoidAsync("localStorage.setItem", key, JsonSerializer.Serialize(value));
+    public static async ValueTask<T> LocalStorage_GetItem<T>(this IJSRuntime JS, string key)
+        => await JS.InvokeAsync<T>("localStorage.getItem", key);
+    public static async ValueTask<T?> LocalStorage_GetDeserializedItem<T>(this IJSRuntime JS, string key)
+    {
+        try
+        {
+            string? item = await JS.InvokeAsync<string?>("localStorage.getItem", key);
+            if (item == null) return default;
+            return JsonSerializer.Deserialize<T>(item);
+        }
+        catch (JsonException)
+        {
+            return default;
+        }
+    }
+}
+
+
---
+++ InvokeCycle/GlobalUsings.cs
@@ -0,0 +1,10 @@
+﻿global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components;
+global using Microsoft.JSInterop;
+global using System.Text.Json.Serialization;
+global using System.Text.Json;
+
+
+
+global using InvokeCycle.CoreUtils.Extensions;
+global using InvokeCycle.Models;
